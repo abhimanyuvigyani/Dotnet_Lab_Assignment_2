9d00375c4d27a9196a8bf075bedca2650cea30fd
abhimanyuvigyani <abhimanyuvigyani@gmail.com>
2026-08-11 18:09:35 +0530

    Add Student class with properties/data members like name,age

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Student2
+{
+    public class Student
+    {
+        private int id = 101;
+        protected string name = "Kartik";
+        protected internal string course = "MCA";
+        public int age = 22;
+    }
+}
